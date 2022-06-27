8f6d17ad96049a521b7f3764c66ef97b5f00c701
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-06-27 13:37:18 -0400

    commit tree 1879fa05d2ab92177c886b2a9610277d00ccc60f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      specific_students = Student.where(first_name: params[:name].capitalize).or  (Student.where(last_name: params[:name].capitalize))
+      render json: specific_students
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
+
 
 end
